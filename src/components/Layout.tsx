0e465bcbfbf64fb6c9d00df7b8b490322d36d84a
Myk <mykcryptodev@gmail.com>
2024-08-20 19:30:04 -0400

    fix font

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -8,13 +8,15 @@ type Props = {
 
 export const Layout: FC<Props> = ({ children }) => {
   return (
-    <div className="flex flex-col gap-2 max-w-2xl mx-auto">
-      <div className="flex items-center justify-between w-full max-w-7xl mx-auto my-4">
-        <h1 className="text-2xl font-bold">Split The Bill</h1>
-        <Wallet />
-      </div>
-      <div className="container mx-auto">
-        {children}
+    <div className="w-full h-full min-h-screen min-w-screen bg-gradient-to-b from-[#d1b3f7] to-[#b1bafd]">
+      <div className="flex flex-col gap-2 max-w-2xl mx-auto">
+        <div className="flex items-center justify-between w-full max-w-7xl mx-auto my-4">
+          <h1 className="text-2xl font-bold">Split The Bill</h1>
+          <Wallet />
+        </div>
+        <div className="container mx-auto">
+          {children}
+        </div>
       </div>
     </div>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,14 +11,14 @@ export default function Home() {
         <meta name="description" content="Split the bill with your friends" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+      <main className="flex min-h-screen flex-col items-center justify-center">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-          <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+          <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
             Split <span className="text-[hsl(280,100%,70%)]">The</span> Bill
           </h1>
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
             <Link
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
+              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
               href="/create"
             >
               <h3 className="text-2xl font-bold">Create New Split</h3>
@@ -28,7 +28,7 @@ export default function Home() {
               </div>
             </Link>
             <Link
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
+              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
               href="/split/join"
             >
               <h3 className="text-2xl font-bold">Pay Split →</h3>
